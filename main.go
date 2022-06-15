ccd69f22b4ed6bdc4436f5ff96c71ca5adc8c60b
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-06-15 17:29:39 -0400

    NewTestCluster

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/vault"
+)
+
+func main() {
+	cluster := vault.NewTestCluster(nil, nil, &vault.TestClusterOptions{
+		HandlerFunc: func(props *vault.HandlerProperties) http.Handler {
+			fmt.Println("hi")
+		},
+	})
+
+	cluster.Start()
+	defer cluster.Cleanup()
+}
