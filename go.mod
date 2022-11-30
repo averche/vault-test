bced16554940b10cd862af388ff910f71ee1d09c
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-11-30 18:29:27 -0500

    approle

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/averche/vault-test
 
 go 1.15
 
-require github.com/hashicorp/vault d45f908ff356f689301eeef63f4cf989fe51c046
+require (
+	github.com/hashicorp/vault v1.5.10-0.20220616165925-69c42adca656
+	github.com/hashicorp/vault-client-go v0.0.0-20221122233247-be9509b33bd6 // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
+	"context"
+	"log"
 
-	"github.com/hashicorp/vault/vault"
+	vault "github.com/hashicorp/vault-client-go"
 )
 
 func main() {
-	cluster := vault.NewTestCluster(nil, nil, &vault.TestClusterOptions{
-		HandlerFunc: func(props *vault.HandlerProperties) http.Handler {
-			fmt.Println("hi")
-			return nil
+	ctx := context.Background()
+
+	// prepare a client with default configuration, except for the address
+	client, err := vault.New(
+		vault.WithBaseAddress("http://127.0.0.1:8200"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// authenticate
+	resp, err := client.Auth.PostAuthApproleLogin(ctx, vault.ApproleLoginRequest{
+		RoleId:   "91504982-e048-3702-5dae-d3cde41a8b15",
+		SecretId: "d5a5a11a-ed72-9dfb-d3e7-7be83454348b",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.SetToken(resp.Auth.ClientToken)
+
+	// write a secret
+	_, err = client.Write(ctx, "/secret/data/my-secret", map[string]interface{}{
+		"data": map[string]interface{}{
+			"password1": "abc123",
+			"password2": "trustno1",
 		},
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("secret written succesffully")
 
-	cluster.Start()
-	defer cluster.Cleanup()
+	// read a secret
+	r, err := client.Read(ctx, "/secret/data/my-secret")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("secret retrieved:", r.Data["data"])
 }
