2eecd5b3b8f35414ad3b1694ed04f798ab2e62cf
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-12-02 14:04:54 -0500

    example from docs

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	ctx := context.Background()
 
-	// prepare a client with default configuration, except for the address
+	// prepare a client with the given base address
 	client, err := vault.New(
 		vault.WithBaseAddress("http://127.0.0.1:8200"),
 	)
@@ -18,21 +18,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// authenticate
-	resp, err := client.Auth.PostAuthApproleLogin(ctx, vault.ApproleLoginRequest{
-		RoleId:   "91504982-e048-3702-5dae-d3cde41a8b15",
-		SecretId: "d5a5a11a-ed72-9dfb-d3e7-7be83454348b",
-	})
-	if err != nil {
+	// authenticate with a root token (insecure)
+	if err := client.SetToken("my-token"); err != nil {
 		log.Fatal(err)
 	}
-	client.SetToken(resp.Auth.ClientToken)
 
 	// write a secret
-	_, err = client.Write(ctx, "/secret/data/my-secret", map[string]interface{}{
-		"data": map[string]interface{}{
+	_, err = client.Write(ctx, "/secret/data/my-secret", map[string]any{
+		"data": map[string]any{
 			"password1": "abc123",
-			"password2": "trustno1",
+			"password2": "correct horse battery staple",
 		},
 	})
 	if err != nil {
