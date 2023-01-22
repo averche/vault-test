8830a7070446c9595793d6245fa01422ef19a687
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-01-21 20:28:32 -0500

    test with api + vault

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"github.com/hashicorp/vault/vault"
+	"github.com/hashicorp/vault/api"
 )
 
 func main() {
+	// reference vault
 	cluster := vault.NewTestCluster(nil, nil, &vault.TestClusterOptions{
 		HandlerFunc: func(props *vault.HandlerProperties) http.Handler {
-			fmt.Println("hi")
+			fmt.Println("test")
+			return nil
 		},
 	})
 
 	cluster.Start()
 	defer cluster.Cleanup()
+
+	// reference api
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Fatalf(err)
+	}
+
+	raw, err := client.Logical().ReadRawWithContext(context.Background(), "secret/data/foo")
+	if err != nil {
+		log.Fatalf(err)
+	}
+	defer raw.Body.Close()
+
+	b, err := io.ReadAll(raw.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("raw", string(b))
 }
